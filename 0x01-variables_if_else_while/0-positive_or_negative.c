15ee393a309b14950a1988edb599b71c18d119f5
Teddy Waweru <teddywaweru@gmail.com>
2022-10-17 13:33:02 -0700

    init submission

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Determune if a random number is ositive or negative
+ * Retyrn: 0 on success
+ */
+int main (void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n < 0)
+	{
+		printf("%d is %s\n", n, "negative");
+	}
+	else if (n > 0)
+	{
+		printf("%d is %s\n", n, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", n, "zero");
+	}
+	
+	return (0);
+
+
+}
