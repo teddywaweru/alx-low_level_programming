46005ae3404ee01101ba90b69d334ea8c62a4486
Teddy Waweru <teddywaweru@gmail.com>
2022-10-17 22:11:41 -0700

    edit: linting errors

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,18 +2,18 @@
 /**
  *  * main - prints numbers 0-9 without using char or printf/puts,
  *   * and putchar 2x max
- *   (*
+ *   *
  *    * Return: 0 on success
- *     */
+*/
 int main(void)
 {
-		int i = '0';
-
-			while (i <= '9')
-					{
-								putchar(i);
-										i++;
-											}
-				putchar('\n');
-					return (0);
+	int i = '0';
+	
+	while (i <= '9')
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }
