067de714f4f5f6b2a0de70b7b0001f65a8fc7c83
Teddy Waweru <teddywaweru@gmail.com>
2022-10-18 13:09:15 -0700

    print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * print_alphabet - print alphabet frm a to z
+ * Return - None
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <='z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
