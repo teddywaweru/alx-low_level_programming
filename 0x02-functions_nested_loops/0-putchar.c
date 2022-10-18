6150ed38a8e04caf0d384b78335be14f847d2433
Teddy Waweru <teddywaweru@gmail.com>
2022-10-18 12:58:20 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,9 +3,11 @@
  * main - prints the string _putchar
  * Return: 0 on success
  */
-int main(void) {
+int main(void) 
+{
 	char c[] = "_putchar";	
 	int i;
+
 	for (i = 0; i < 8; i++) {
 		_putchar(c[i]);
 	}
