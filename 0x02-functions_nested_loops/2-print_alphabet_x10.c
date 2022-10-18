90cf21049e7130e7b46acf97d7d83cbf34b0f366
Teddy Waweru <teddywaweru@gmail.com>
2022-10-18 13:22:31 -0700

    edit: print alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - Print a to z 10 times
+ * Returns: None
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+	}
+	_putchar('\n');
+}
