b82092b88a8b4d3d19b41c08992228e324b559c9
teddywaweru <teddywaweru@gmail.com>
2022-10-27 01:08:27 +0300

    edit: compilation errors

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -18,13 +18,13 @@ void puts_half(char *str)
 	{
 		for (i = (len / 2); str[i] != '\0'; i++)
 		{
-			_putchar(str[i]);	
+			_putchar(str[i]);
 		}
 	} else if (len % 2)
 	{
-		for (i = ((len - 1) / 2); str[i] != '\0'; i++)
+		for (i = ((len - 1) / 2); i < len; i++)
 		{
-			_putchar(str[i + 1]);	
+			_putchar(str[i + 1]);
 		}
 	}
 	_putchar('\n');
