7aff9dba142a6a338503bf0cb92d5d44a27297b6
teddywaweru <teddywaweru@gmail.com>
2022-10-26 23:17:41 +0300

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * rev_string - Reverse a string
+ * @s: string to be reversed
+ *
+ * Return: None
+ */
+
+void rev_string(char *s)
+{
+	char *string_bak = *s;
+	char store[1000];
+	short idx = 0;
+
+	while (*s != '\n')
+	{
+		store[idx] = *s;
+		s++;
+		idx++;
+	}
+	idx = 0;
+	while (s > t)
+	{
+		s--;
+		*s = store[idx];
+		idx++;
+	}
+}
+
