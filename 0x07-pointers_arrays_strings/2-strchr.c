a45a895dff4216af08cdb8e08f37d860560f109b
teddywaweru <teddywaweru@gmail.com>
2022-11-01 19:34:07 +0300

    _strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strchr - Returns on the first occurrence of a specific character
+ * @s: string of characters
+ * @c: character to be searched for
+ *
+ * Return: Pointer to the first occurrence, or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i,j;
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
+}
