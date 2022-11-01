8962dc2c9acdf7fe9cc928dc01590526f7631df9
teddywaweru <teddywaweru@gmail.com>
2022-11-01 19:49:28 +0300

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - returns initial number of bytes in s that are in accept
+ * @s: string 
+ * @accept: bytes that are wanted
+ * 
+ * Return: Number of bytes
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+	char *t = accept;
+	
+	while (*s++)
+	{
+		while (*accept++)
+		{
+			if (*(s - 1) == *(accept - 1))
+			{
+				i++;
+				break;
+			}
+		}
+		if (!(*--accept))
+			break;
+		accept = t;
+	}
+}
