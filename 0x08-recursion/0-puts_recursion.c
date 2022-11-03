9f57e9e3e89d73f837e4549ca528aa99950e5976
teddywaweru <teddywaweru@gmail.com>
2022-11-03 19:57:16 +0300

    putting with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - Print a string followed by a newline
+ * @s: string to be printed
+ *
+ * Return: None
+ */
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putschar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	else
+	{
+		_putschar('\n');
+	}
+}
---
+++ 0x08-recursion/main.h
@@ -0,0 +1,3 @@
+#include <stdio.h>
+
+_putschar(char *s);
