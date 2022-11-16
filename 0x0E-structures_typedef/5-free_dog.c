054c9c9ff5da3edce704bec748afa529923d66ac
teddywaweru <teddywaweru@gmail.com>
2022-11-16 22:33:30 +0300

    add: free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - free dog structure
+ *@d:dog struct
+ * Return: no return
+ */
+void free_dog(struct dog *d) {
+
+  if (d != NULL) {
+    free(d->name);
+    free(d->owner);
+    free(d);
+  }
+}
