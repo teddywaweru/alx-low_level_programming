8fac0d585c1895079522ef7a1be140095cb2af14
teddywaweru <teddywaweru@gmail.com>
2022-11-16 22:29:19 +0300

    add: create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
 #include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -9,45 +9,39 @@
  *@owner: char
  * Return: new dog
  */
-dog_t *new_dog(char *name, float age, char *owner)
-{
+struct dog *new_dog(char *name, float age, char *owner) {
 
-	char *name2;
-	char *owner2;
-	int n, o, i, j;
-	struct dog *luna;
+  char *name2;
+  char *owner2;
+  int n, o, i, j;
+  struct dog *luna;
 
-	luna = malloc(sizeof(struct dog));
-	if (luna == NULL)
-		return (NULL);
-	for (n = 0; *(name + n) != '\0'; n++)
-		;
-	for (o = 0; *(owner + o) != '\0'; o++)
-		;
-	name2 = malloc(n + 1);
-	if (name2 != NULL)
-	{
-		for (i = 0; i <= n; i++)
-			*(name2 + i) = *(name + i);
-	} else
-	{
-		free(luna);
-		return (NULL);
-	}
-	owner2 = malloc(o + 1);
-	if (owner2 != NULL)
-	{
-		for (j = 0; j <= o; j++)
-			*(owner2 + j) = *(owner + j);
-	} else
-	{
-		free(name2);
-		free(luna);
-		return (NULL);
-	}
-	luna->name = name2;
-	luna->age = age;
-	luna->owner = owner2;
-	return (luna);
-
+  luna = malloc(sizeof(struct dog));
+  if (luna == NULL)
+    return (NULL);
+  for (n = 0; *(name + n) != '\0'; n++)
+    ;
+  for (o = 0; *(owner + o) != '\0'; o++)
+    ;
+  name2 = malloc(n + 1);
+  if (name2 != NULL) {
+    for (i = 0; i <= n; i++)
+      *(name2 + i) = *(name + i);
+  } else {
+    free(luna);
+    return (NULL);
+  }
+  owner2 = malloc(o + 1);
+  if (owner2 != NULL) {
+    for (j = 0; j <= o; j++)
+      *(owner2 + j) = *(owner + j);
+  } else {
+    free(name2);
+    free(luna);
+    return (NULL);
+  }
+  luna->name = name2;
+  luna->age = age;
+  luna->owner = owner2;
+  return (luna);
 }
