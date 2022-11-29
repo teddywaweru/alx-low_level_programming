c7f87988b29523d8ee54b4acb396d6595064666f
teddywaweru <teddywaweru@gmail.com>
2022-11-29 22:49:47 +0300

    add: solutions

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print elements of a list_t list
+ * @h: first element of the list
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h) {
+  int i;
+
+  i = 0;
+  while (h != NULL) {
+    if (h->str == NULL) {
+      printf("[%d] %s\n", 0, "(nil)");
+      h = h->next;
+      i++;
+    } else {
+      printf("[%d] %s", h->len, h->str);
+      i++;
+      h = h->next;
+      printf("\n");
+    }
+  }
+  return (i);
+}
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list
+ * @head: first element of the list
+ * Return: no return
+ */
+void free_list(list_t *head) {
+  struct list_s *temp = NULL;
+  struct list_s *aux = NULL;
+
+  temp = head;
+  while (temp != NULL) {
+    aux = temp->next;
+    free(temp->str);
+    free(temp);
+    temp = aux;
+  }
+  head = NULL;
+}
