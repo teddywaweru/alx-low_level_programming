5adde4534b627e7f5c19927021b14c52712f86e0
teddywaweru <teddywaweru@gmail.com>
2022-12-07 20:25:13 +0300

    add: fix errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,50 +1,49 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
- * _pow - Get the second exponential of a number
- * @num: power number
+/**
+ * _pow - pow to 2
+ * @num: number
  * @n: number of times
- *
- * Return: num^n
+ * Return: result to pow to  n number
  */
 unsigned int _pow(unsigned int num, unsigned int n) {
   unsigned int sum, i;
-  sum = 1;
 
+  sum = 1;
   for (i = 0; i < n; i++) {
     sum = sum * num;
   }
-
   return (sum);
 }
 
 /**
- * binary_to_uint - Convert binary number to unsigned int
- * @b: string
- *
- * Return: The converted number or 0
+ * binary_to_uint - convert Binary to uint
+ * @b: binary number
+ * Return: the converted number or O
  */
 unsigned int binary_to_uint(const char *b) {
+
   unsigned int decimal, i, k;
 
   decimal = 0;
   i = 0;
+  decimal = 0;
   if (b == NULL)
     return (0);
-
   for (k = 0; b[k] != '\0'; k++) {
-    if ((b[k] != '0') && (b[k] != '1'))
+    if (((b[k]) != '0') && ((b[k]) != '1') && ((b[k]) != '\0'))
       return (0);
   }
-  k -= 1;
+  k = k - 1;
   while (b[i] != '\0') {
-    if (b[k - 1] == '1') {
+    if ((b[k - i]) == '1') {
       if (i == 0)
         decimal += 1;
       else
         decimal += _pow(2, i);
     }
+    i++;
   }
   return (decimal);
 }
